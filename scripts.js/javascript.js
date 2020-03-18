bb3bb57c99631a489e75f87a58f5a6a0114b7acc
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 10:42:16 -0500

    .

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -1,1 +1,15 @@
-console.log("in JS");
+$(document).ready(init);
+
+function init() {
+    $('.js-btn-create-switch').on('click', createColorSwitch);
+
+}
+
+createColorSwitch() {
+    $('.js-container').append(`
+    <div>
+    <p>Switch</p>
+    <button>Turn Off</button>
+    <div>
+    `);
+}
