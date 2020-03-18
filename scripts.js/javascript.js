d8b5b5e860ec6369c251b82be25186a973ae1f1e
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 14:33:47 -0500

    something

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -7,7 +7,7 @@ let switchesOn = 0;
 // add event listener call to Turn Off Btn to turn off switch- target container with filter class (btn) (on when page loads)
 function init() {
   $(".js-btn-create-switch").on("click", createColorSwitch);
-  //   $(".js-container").on("click, .turnOff", turnSwitchOff);
+  $(".js-container").on("click", ".js-btn-turnOff", turnSwitchOff);
 }
 
 // target div with function: create <p>, create <btn> (inside div)
@@ -15,9 +15,9 @@ function init() {
 // make turn off btn turn off switch by adding a class to it and target the class.
 function createColorSwitch() {
   $(".js-container").append(`
-    <div class="onStyle">   
+    <div class="on">   
     <p>Switch</p>
-    <button class="turnOff">Turn Off</button>
+    <button class="js-btn-turnOff">Turn Off</button>
     <div>
     `);
   // create switches on counter & (add global variable) & call
@@ -30,5 +30,11 @@ function updateSwitchCounter() {
   $(".js-switches-h1-count").text(`Switches turned on: ${switchesOn}`);
 }
 
-// // create function to turn off switch when Turn Off btn is clicked (event lister added)
-// function turnSwitchOff() {}
+// // create function to turn off switch when Turn Off btn is clicked
+// (event lister added in init)
+// append <TurnOff Btn> parent class by adding class off off (& style)
+function turnSwitchOff() {
+  $(this)
+    .parent()
+    .addClass("off");
+}
