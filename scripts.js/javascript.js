87a8951d2d999a596ab2083f5f26dfd26741342e
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 18:44:18 -0500

    created switchCOunter variable & CalculateSwitchCOunt function. Testing AOK

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -10,6 +10,16 @@ function turnSwitchOn() {
     <p> This is a Switch!<p>
     <button>Turn Switch Off</button>
     <div>
-    
     `);
+
+  switchCounter++;
+  calculateSwitchCount();
+}
+
+let switchCounter = 0;
+
+function calculateSwitchCount() {
+  $(".js-switches-turned-on-count").text(`Switches Turned On: ${switchCounter}
+  
+  `);
 }
