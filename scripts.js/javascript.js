548bff749de877dd9bf3a2ba9a8441f767b9d72d
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 10:55:59 -0500

    notes added

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -1,12 +1,13 @@
 $(document).ready(init);
 
+// on init: target btn on html.  on click: target container div with function.
 function init() {
-    $('.js-btn-create-switch').on('click', createColorSwitch);
-
+  $(".js-btn-create-switch").on("click", createColorSwitch);
 }
 
-createColorSwitch() {
-    $('.js-container').append(`
+// target div, create <p>, create <btn> (inside div)
+function createColorSwitch() {
+  $(".js-container").append(`
     <div>
     <p>Switch</p>
     <button>Turn Off</button>
