fc9483c0c9ec1968be7ca16e4749bd0ea4230131
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 14:56:32 -0500

    html created.  Made ready, added event listener to html btn, and ceated div to append with createSwitch function

---
+++ scripts.js/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(init);
+
+function init() {
+  $("js-btn-create-switch").on('click', createSwitch);
+}
+
+function createSwitch(
+    $('.js-container').append(`
+    <div>
+        <p>This is a switch<p>
+        <button>Turn Switch Off</button>
+    <div>
+    
+    `);
+
+}
