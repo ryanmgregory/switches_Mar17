2d10a00960391a6261c16299a019eb15483d19f2
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 11:38:49 -0500

    switches on variable created

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -1,6 +1,8 @@
 //ready document with init function
 $(document).ready(init);
 
+let switchesOn = 0;
+
 // on init: target btn on html.  on click: target container div with function.
 function init() {
   $(".js-btn-create-switch").on("click", createColorSwitch);
@@ -15,4 +17,8 @@ function createColorSwitch() {
     <button>Turn Off</button>
     <div>
     `);
+  // create switches on counter & (add global variable)
+  switchesOn++;
 }
+
+// create counter to target <h1> when create switch is clicked
