7ebd229f58680e647a4bc534108351d071a7a51e
Ryan Gregory <RyanGregoryCodes@gmail.com>
2020-03-18 19:03:52 -0500

    turnSwitchOff fucntion created. Targeting parent of BTN not working

--- scripts.js/javascript.js
+++ scripts.js/javascript.js
@@ -2,13 +2,14 @@ $(document).ready(init);
 
 function init() {
   $(".js-btn-create-switch").on("click", turnSwitchOn);
+  $(".js-container").on("click", ".js-btn-turn-off", turnSwitchOff);
 }
 
 function turnSwitchOn() {
   $(".js-container").append(`
     <div class="on">
     <p> This is a Switch!<p>
-    <button>Turn Switch Off</button>
+    <button class="js-btn-turn-off">Turn Switch Off</button>
     <div>
     `);
 
@@ -21,4 +22,14 @@ let switchCounter = 0;
 function calculateSwitchCount() {
   $(".js-switches-turned-on-count").text(`Switches Turned On: ${switchCounter}
   `);
+}
+
+// create Turn Off Switch function & target the Turn Switch Off BTN
+function turnSwitchOff() {
+  $(this)
+    .parent()
+    .addClass("off");
+
+  switchCounter--;
+  calculateSwitchCount();
 }
